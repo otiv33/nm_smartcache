7e1a9fcbb1d5207bbe8c80a85bc77b71f78255b2
Vito Abeln <vito.abeln@gmail.com>
2023-12-06 14:33:20 +0100

    Added API endpoints with supporting functions

---
+++ smartcache.API/Controllers/EmailsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using smartcache.API.Models;
+
+namespace smartcache.API.Controllers
+{
+    [Route("emails")]
+    [ApiController]
+    public class EmailsController : ControllerBase
+    {
+        private readonly IGrainFactory grains;
+        public EmailsController() {
+        
+        }
+
+        [HttpGet]
+        [Route("{email}")]
+        public IResult CheckEmail(string email)
+        {
+            if (!EmailHelper.IsValidEmail(email))
+            {
+                return Results.BadRequest();
+            }
+
+            Email parsedEmail = EmailHelper.ParseEmail(email);
+
+            return Results.Ok();
+        }
+
+        [HttpPost]
+        [Route("{emai}")]
+        public IResult AddEmail(string email)
+        {
+            if (!EmailHelper.IsValidEmail(email))
+            {
+                return Results.BadRequest();
+            }
+
+            Email parsedEmail = EmailHelper.ParseEmail(email);
+
+            return Results.Ok();
+        }
+
+    }
+}
