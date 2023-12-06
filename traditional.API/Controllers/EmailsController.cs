a1873f8d87da418f7b9dceb3a1f924f23629c3c5
Vito Abeln <vito.abeln@gmail.com>
2023-12-06 19:05:15 +0100

    Added traditional API with DB example

---
+++ traditional.API/Controllers/EmailsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using traditional.API.Models;
+
+namespace traditional.API.Controllers
+{
+    [Route("emails")]
+    [ApiController]
+    public class EmailsController : ControllerBase
+    {
+        private readonly EmailStoreDbContext _context;
+        public EmailsController(EmailStoreDbContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("{email}")]
+        public async Task<IResult> CheckEmail(string email)
+        {
+            Email parsedEmail = ParseEmail(email);
+            Email? res = _context.Emails.Where(x => x.Domain == parsedEmail.Domain && x.LocalPart == parsedEmail.LocalPart).FirstOrDefault();
+            return res != null ? Results.Ok("OK") : Results.NotFound("Not found");
+        }
+
+        [HttpPost]
+        [Route("{email}")]
+        public Task<IResult> AddEmail(string email)
+        {
+            Email parsedEmail = ParseEmail(email);
+            Email? res = _context.Emails.Where(x => x.Domain == parsedEmail.Domain && x.LocalPart == parsedEmail.LocalPart).FirstOrDefault();
+            if (res != null)
+            {
+                _context.Emails.Add(parsedEmail);
+                return Task.FromResult(Results.Created("Created", email));
+            }
+            else
+            {
+                return Task.FromResult(Results.Conflict("Email already exists"));
+            }
+        }
+
+        public static Email ParseEmail(string email)
+        {
+            var split = email.Split("@");
+            return new Email(split[0], split[1]);
+        }
+    }
+}
---
+++ traditional.API/EmailStore.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using traditional.API.Models;
+
+
+namespace traditional.API
+{
+    public partial class EmailStoreDbContext : DbContext
+    {
+        public virtual DbSet<Email> Emails { get; set; }
+
+        public EmailStoreDbContext()
+        {
+        }
+
+        public EmailStoreDbContext(DbContextOptions<EmailStoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    }
+}
+
